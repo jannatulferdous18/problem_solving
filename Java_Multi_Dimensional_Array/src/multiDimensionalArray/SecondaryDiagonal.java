e426f0af3088ef1b3f99e4bc907e7df57965b3a7
macbook <macbook@192.168.0.103>
2021-01-23 23:15:34 +0100

    find Secondary/ Anti-diagonal/ Counter Diagonal in 2-D array

---
+++ Java_Multi_Dimensional_Array/src/multiDimensionalArray/SecondaryDiagonal.java
@@ -0,0 +1,32 @@
+package multiDimensionalArray;
+
+import java.util.Scanner;
+
+public class SecondaryDiagonal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the row number:");
+		int row = sc.nextInt();
+		System.out.println("Enter the coloumn number:");
+		int column = sc.nextInt();
+		int twoD_arr[][] = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				twoD_arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print("The matrix you entered: \n");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(twoD_arr[i][j] + "    ");
+			}
+			System.out.println();
+		}
+		System.out.print("Secondary/ Anti/ Counter Diagonal: ");
+		for (int i = 0; i < row; i++) {
+			System.out.print(twoD_arr[i][row - i - 1] + " ");
+		}
+	}
+
+}
