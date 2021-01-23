9e7d47766241be020ab85636c5d17e22212a8c28
macbook <macbook@192.168.0.103>
2021-01-23 22:00:56 +0100

    find the minimum value of each column of a 2-D array

---
+++ Java_Multi_Dimensional_Array/src/multiDimensionalArray/minimumValueEachColumn.java
@@ -0,0 +1,37 @@
+package multiDimensionalArray;
+
+import java.util.Scanner;
+
+public class minimumValueEachColumn {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the row number:");
+		int row = sc.nextInt();
+		System.out.println("Enter the coloumn number:");
+		int column = sc.nextInt();
+		int twoD_arr[][] = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				twoD_arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print("The matrix you entered: \n");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(twoD_arr[i][j] + "    ");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < column; i++) {
+			int min = twoD_arr[0][i];
+			for (int j = 1; j < row; j++) {
+				if (twoD_arr[j][i] < min) {
+					min = twoD_arr[j][i];
+				}
+			}
+			System.out.println("Minimum value of column" + i + " is " + min);
+		}
+	}
+
+}
