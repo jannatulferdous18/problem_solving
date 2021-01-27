10d8548c471b2d57eaf2b0c67a5c038594d02737
macbook <macbook@192.168.0.101>
2021-01-27 20:13:21 +0100

    Find Upper Triangular Matrix in two dimensional(2D) array

---
+++ Java_Multi_Dimensional_Array/src/multiDimensionalArray/UpperTriangleMatrix.java
@@ -0,0 +1,46 @@
+package multiDimensionalArray;
+
+import java.util.Scanner;
+
+public class UpperTriangleMatrix {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the row number:");
+		int row = sc.nextInt();
+		System.out.println("Enter the coloumn number:");
+		int column = sc.nextInt();
+		int twoD_arr[][] = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				twoD_arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print("The matrix you entered: \n");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(twoD_arr[i][j] + "    ");
+			}
+			System.out.println();
+		}
+		int check_upperTriangle;
+		int flag = 0;
+		for (int i = 1; i < row; i++) {
+			for (int j = 0; j < i; j++) {
+				check_upperTriangle = twoD_arr[i][j];
+				if (check_upperTriangle == 0) {
+					flag = 1;
+				} else {
+					break;
+				}
+			}
+		}
+		if (flag == 1) {
+			System.out.println("\nUpper Triangular Matrix");
+		} else {
+			System.out.println("\nNot Triangular Matrix");
+
+		}
+	}
+
+}
